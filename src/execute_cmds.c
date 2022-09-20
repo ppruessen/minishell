f7bfc9d8f728e0120bc4370cd7b37ac458d35660
MelSchi <mail-mel@posteo.de>
2022-09-20 15:45:27 +0200

    pipes changed

--- src/execute_cmds.c
+++ src/execute_cmds.c
@@ -6,7 +6,7 @@
 /*   By: mschiman <mschiman@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 14:43:18 by mschiman          #+#    #+#             */
-/*   Updated: 2022/09/20 13:56:46 by mschiman         ###   ########.fr       */
+/*   Updated: 2022/09/20 15:43:38 by mschiman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,8 +80,8 @@ void	execute_cmd(t_cmd *cmd, t_var *var, int i)
 
 	set_cmd_path(var->cmds[i], var);
 	var->env = create_env_from_list(var->env_list);
-	if (var->pipes > 0)
-		set_pipes_in_child(var, cmd, i);
+//	if (var->pipes > 0)
+//		set_pipes_in_child(var, cmd, i);
 	open_redirections(cmd);
 	execve_input = NULL;
 	input_members = 0;
@@ -108,13 +108,98 @@ void	execute_cmds(t_var *var)
 {
 	int		i;
 	pid_t	pid;
+//	int		fds[2];
+	int saved_stdin;
 
-	if (var->pipes > 0)
-		open_filedescriptors(var);
+
+
+//	if (var->pipes > 0)
+//		open_filedescriptors(var);
 	i = 0;
 	if (var->cmds == NULL)
 		return ;
-	while (var->cmds[i - 1] != NULL)
+	saved_stdin = fcntl(STDIN_FILENO, F_DUPFD_CLOEXEC, 0);
+	while (var->cmds[i + 1] != NULL)
+	{
+		int		fds[2];
+		if (pipe(fds) < 0)
+		{
+			perror("pipe(2) error");
+			break ;
+		}
+		if ((pid = fork()) < 0)
+		{
+			perror("fork(2) error");
+			break ;
+		}
+        /* Parent needs to close the pipe's write channel to make sure
+         * we don't hang. Parent reads from the pipe's read channel.
+         */
+		if (pid > 0)
+		{
+			if (close(fds[WRITE]) < 0)
+			{
+				perror("close(2) error");
+				break;
+			}
+			if (dup2(fds[READ], STDIN_FILENO) < 0)
+			{
+				perror("dupPipe() error");
+				break;
+			}
+		}
+		/* Child Process */
+		else
+		{
+//			if (var->cmds[i]->read_from_file == TRUE)
+				open_read_from_file(var->cmds[i]);
+//			if (var->cmds[i]->write_to_file == TRUE)
+				open_write_to_file(var->cmds[i]);
+			if (close(fds[READ]) < 0)
+			{
+				perror("close(2) error");
+				break;
+			}
+			if (dup2(fds[WRITE], STDOUT_FILENO) < 0)
+			{
+				perror("dupPipe() error");
+				break;
+			}
+		}
+		i++;
+	}
+	while (var->cmds[i] != NULL)
+	{
+		printf("Zweite While loop 173.\n");
+		open_read_from_file(var->cmds[i]);
+		open_write_to_file(var->cmds[i]);
+		if (catch_inbuilts(var, i) == 0)
+			break ;
+		else if (catch_inbuilts(var, i) == 1)
+			i++;
+		pid = fork();
+		if (pid == 0)
+			execute_cmd(var->cmds[i], var, i);
+		// else
+		// {
+		// 	if (var->pipes > 0)
+		// 		close_pipes_in_parent(var, i);
+		// }
+		i++;
+	}
+	wait_function(pid);
+	if (dup2(saved_stdin, STDIN_FILENO) < 0)
+		perror("dup2(2) error when attempting to restore stdin");
+	if (close(saved_stdin) < 0)
+	{
+		perror("close(2) failed on saved_stdin");
+	}
+}
+
+
+
+/*		
+	while (var->cmds[i + 1] != NULL)
 	{
 		if (catch_inbuilts(var, i) == 0)
 			break ;
@@ -132,3 +217,4 @@ void	execute_cmds(t_var *var)
 	}
 	wait_function(pid);
 }
+*/
